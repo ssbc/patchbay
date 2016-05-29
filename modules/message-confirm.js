51bb7f2c4da0239b8114b4f425ca3da89741c261
Dominic Tarr <dominic.tarr@gmail.com>
2016-05-29 23:17:08 +1200

    enter to post message, esc to cancel

--- modules/message-confirm.js
+++ modules/message-confirm.js
@@ -11,21 +11,45 @@ exports.message_confirm = function (content, sbot) {
   var lb = lightbox()
   document.body.appendChild(lb)
 
+  var okay = h('button', 'okay', {onclick: function () {
+    u.firstPlug(exports.publish, content, null, sbot)
+    lb.remove()
+  }})
+
+  var cancel = h('button', 'cancel', {onclick: function () {
+    lb.remove()
+  }})
+
+  var hidden = h('input', {style: {visible: 'hidden'}})
+
+
+  okay.addEventListener('keydown', function (ev) {
+    if(ev.keyCode === 27) cancel.click() //escape
+  })
+
   lb.show(h('div.column',
     u.firstPlug(exports.message_content,
       {key: "DRAFT", value: {content: content}}, sbot
     ) || h('pre', JSON.stringify(content, null, 2)),
-    h('div.row',
-      h('button', 'okay', {onclick: function () {
-        u.firstPlug(exports.publish, content, null, sbot)
-        lb.remove()
-      }}),
-      h('button', 'cancel', {onclick: function () {
-        lb.remove()
-      }})
-    )
+    h('div.row', okay, cancel)
   ))
 
+  okay.focus()
+
+//  hidden.focus()
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
