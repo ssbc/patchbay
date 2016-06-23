5e55c4b6b3322e83b1d154ca6044fcc656c1bb2c
Dominic Tarr <dominic.tarr@gmail.com>
2016-06-24 00:34:04 +1200

    automatically reconnect sbot api

--- sbot-api.js
+++ sbot-api.js
@@ -1,5 +1,6 @@
 var pull = require('pull-stream')
 var crypto = require('crypto')
+var Reconnect = require('pull-reconnect')
 
 function Hash (onHash) {
   var hash = crypto.createHash('sha256')
@@ -16,36 +17,49 @@ function Hash (onHash) {
 }
 
 
-module.exports = function (sbot) {
+var createClient = require('ssb-client')
+
+module.exports = function () {
+  var sbot = null
+  var rec = Reconnect(function (isConn) {
+    createClient(function (err, _sbot) {
+      if(err) return isConn(err)
+      sbot = _sbot
+      sbot.on('closed', function () {
+        isConn(new Error('closed'))
+      })
+      isConn()
+    })
+  })
 
   return {
-    sbot_blobs_add: function (cb) {
+    sbot_blobs_add: rec.sink(function (cb) {
       return pull(
         Hash(cb),
         sbot.blobs.add()
       )
-    },
-    sbot_links: function (query) {
+    }),
+    sbot_links: rec.source(function (query) {
       return sbot.links(query)
-    },
-    sbot_links2: function (query) {
+    }),
+    sbot_links2: rec.source(function (query) {
       return sbot.links2.read(query)
-    },
-    sbot_log: function (opts) {
+    }),
+    sbot_log: rec.source(function (opts) {
       return sbot.createLogStream(opts)
-    },
-    sbot_user_feed: function (opts) {
+    }),
+    sbot_user_feed: rec.source(function (opts) {
       return sbot.createUserStream(opts)
-    },
-    sbot_get: function (key, cb) {
+    }),
+    sbot_get: rec.async(function (key, cb) {
       sbot.get(key, cb)
-    },
-    sbot_publish: function (msg, cb) {
+    }),
+    sbot_publish: rec.async(function (msg, cb) {
       sbot.publish(msg, cb)
-    },
-    sbot_whoami: function (cb) {
+    }),
+    sbot_whoami: rec.async(function (cb) {
       sbot.whoami(cb)
-    }
+    })
   }
 }
 
