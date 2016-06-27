937c17f654fecfdf321c68504c55d724b112fb94
Charles Lehner <cel@celehner.com>
2016-06-27 10:42:37 -0400

    Handle errors related to avatars

--- modules/names.js
+++ modules/names.js
@@ -39,8 +39,10 @@ function name (id) {
       //to patchwork style naming (i.e. self id)
       if(!names.length)
         return sbot_whoami(function (err, me) {
+          if (err) return console.error(err)
           getAvatar({links: sbot_links}, me.id, id,
             function (err, avatar) {
+              if (err) return console.error(err)
               console.log(avatar)
               n.textContent = avatar.name
             })
