29997160555aa9df4541f65ba3ca7b58d4a8c9c4
Charles Lehner <cel@celehner.com>
2016-06-27 22:42:54 -0400

    Add channel message_meta and screen_view

---
+++ modules/channel.js
@@ -0,0 +1,55 @@
+var h = require('hyperscript')
+var u = require('../util')
+var pull = require('pull-stream')
+var Scroller = require('pull-scroll')
+
+var plugs = require('../plugs')
+var message_render = plugs.first(exports.message_render = [])
+var message_compose = plugs.first(exports.message_compose = [])
+var sbot_log = plugs.first(exports.sbot_log = [])
+
+exports.message_meta = function (msg) {
+  var chan = msg.value.content.channel
+  if (chan)
+    return h('a', {href: '##'+chan}, '#'+chan)
+}
+
+exports.screen_view = function (path) {
+  if(path[0] === '#') {
+    var channel = path.substr(1)
+
+    var content = h('div.column.scroller__content')
+    var div = h('div.column.scroller',
+      {style: {'overflow':'auto'}},
+      h('div.scroller__wrapper',
+        message_compose({type: 'post'}), //header
+        content
+      )
+    )
+
+    function matchesChannel(msg) {
+      if (msg.sync) console.error('SYNC', msg)
+      var c = msg && msg.value && msg.value.content
+      return c && c.channel === channel
+    }
+
+    pull(
+      sbot_log({old: false}),
+      pull.filter(matchesChannel),
+      Scroller(div, content, message_render, true, false)
+    )
+
+    pull(
+      /*
+      sbot_query({query: [
+        {$filter: {value: {content: {channel: channel}}}}
+      ]}),
+      */
+      u.next(sbot_log, {reverse: true, limit: 100, live: false}),
+      pull.filter(matchesChannel),
+      Scroller(div, content, message_render, false, false)
+    )
+
+    return div
+  }
+}
