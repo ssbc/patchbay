18524fccf4dfb71af2ebb049d12b40ba308d6e1b
Charles Lehner <cel@celehner.com>
2016-07-10 03:01:49 -0400

    Periodically update relative time strings

--- modules/timestamp.js
+++ modules/timestamp.js
@@ -1,6 +1,19 @@
 var h = require('hyperscript')
 var moment = require('moment')
 
+function updateTimestampEl(el) {
+  el.firstChild.nodeValue = moment(el.timestamp).fromNow()
+  return el
+}
+
+setInterval(function () {
+  var els = [].slice.call(document.querySelectorAll('.timestamp'))
+  els.forEach(updateTimestampEl)
+}, 60e3)
+
 exports.message_meta = function (msg) {
-  return h('a.enter', {href: '#'+msg.key}, moment(msg.value.timestamp).fromNow())
+  return updateTimestampEl(h('a.enter.timestamp', {
+    href: '#'+msg.key,
+    timestamp: msg.value.timestamp
+  }, ''))
 }
