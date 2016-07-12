95e8b51e4818c8911c84165d1cb8a666d8921444
Charles Lehner <cel@celehner.com>
2016-07-11 23:19:18 -0400

    Fix about message links

--- modules/about.js
+++ modules/about.js
@@ -2,7 +2,7 @@
 var h = require('hyperscript')
 
 function idLink (id) {
-  return h('a', {href:'#/'+id}, id)
+  return h('a', {href:'#'+id}, id)
 }
 
 exports.message_content = function (msg, sbot) {
@@ -18,7 +18,7 @@ exports.message_content = function (msg, sbot) {
       ? h('span', 'self-identifies') 
       : h('span', 'identifies ', idLink(id)),
     ' as ',
-    h('a', {href:"#/"+about.about},
+    h('a', {href:"#"+about.about},
       about.name || null,
       about.image
       ? h('img', {src:'http://localhost:7777/'+ encodeURIComponent(about.image.link)})
