b584acdb7216737141340d6bd53a13659b7b88cc
Dominic Tarr <dominic.tarr@gmail.com>
2016-07-15 13:10:47 +1200

    update to depject@1

--- build.js
+++ build.js
@@ -1,13 +1,15 @@
 
 console.log(
-  'module.exports = [\n',
+  'module.exports = {\n'
+  +
   require('fs').readdirSync(require('path').join(__dirname, 'modules'))
   .filter(function (file) {
     return file !== 'index.js'
   })
   .map(function (file) {
-    return "  require('./"+file+"')"
-  }).join(',\n'),
-  '\n]'
+    return '  '+JSON.stringify(file) + ":  require('./"+file+"')"
+  }).join(',\n')
+  +
+  '\n}'
 )
 
--- index.js
+++ index.js
@@ -6,29 +6,34 @@ var fs = require('fs')
 var path = require('path')
 var SbotApi = require('./sbot-api')
 
-document.head.appendChild(
-  h('style', fs.readFileSync('./style.css', 'utf8')
-))
-
 var modules = require('./modules')
 
 var renderers = []
 var app = []
-var App = require('./plugs').first(app)
+//var App = require('./plugs').first(app)
 
 var u = require('./util')
 
-modules.unshift(SbotApi())
-modules.unshift({app: app})
-combine(modules)
+//modules.unshift(SbotApi())
+//modules.unshift({app: app})
 
-document.body.appendChild(App())
+//var indexes = fs.readdirSync(path.join(__dirname, 'modules'))
+//var i = indexes.indexOf('index.js')
+//indexes.splice(i, 1)
 
+modules['app.js' ] = {app: app}
+modules['sbot-api.js'] = SbotApi()
 
+combine(modules) //, ['app', 'sbot'].concat(indexes) )
 
+if(process.title === 'node') {
+  console.log(require('depject/graph')(modules))
+  process.exit(0)
+}
 
+document.head.appendChild(
+  h('style', fs.readFileSync('./style.css', 'utf8')
+))
 
-
-
-
+document.body.appendChild(modules['app.js'].app[0]())
 
