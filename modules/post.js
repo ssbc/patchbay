b8c0ac9c32b89a9bd44ff026a5f5c5cd4368ffbe
Dominic Tarr <dominic.tarr@gmail.com>
2016-07-21 16:54:52 +1200

    use markdown plug

--- modules/post.js
+++ modules/post.js
@@ -7,30 +7,24 @@ var ref = require('ssb-ref')
 
 var plugs = require('../plugs')
 var message_link = plugs.first(exports.message_link = [])
+var markdown = plugs.first(exports.markdown = [])
 
-exports.message_content = function (data, sbot) {
+exports.message_content = function (data) {
   if(!data.value.content || !data.value.content.text) return
 
   var root = data.value.content.root
   var re = !root ? null : h('span', 're: ', message_link(root))
 
-  var content = h('div')
-  var d = h('div', re, content)
+  return h('div',
+    re,
+    markdown(data.value.content)
+  )
 
-  var mentions = {}
-  if(Array.isArray(data.value.content.mentions))
-  data.value.content.mentions.forEach(function (link) {
-    if(link.name) mentions["@"+link.name] = link.link
-  })
-
-  content.innerHTML =
-    markdown.block(data.value.content.text, {toUrl: function (id) {
-      if(ref.isBlob(id))
-        return 'http://localhost:7777/'+encodeURIComponent(id)
-      return '#'+(mentions[id]?mentions[id]:id)
-    }})
-  return d
 }
+
+
+
+
 
 
 
