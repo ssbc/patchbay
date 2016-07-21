ae04202f98d04cb5fc34599b6ffa72a4936080b3
Dominic Tarr <dominic.tarr@gmail.com>
2016-07-21 16:44:35 +1200

    separate markdown into it's own plugin

--- modules/markdown.js
+++ modules/markdown.js
@@ -1,7 +1,31 @@
+var markdown = require('ssb-markdown')
+var h = require('hyperscript')
+var ref = require('ssb-ref')
+//
 
-//
-//exports.markdown = function (body, msg
-//
-//function (text, ref, title) {
-//  return <h
-//}
+var blob_url = require('../plugs').first(exports.blob_url = [])
+
+exports.markdown = function (content) {
+  if('string' === typeof content)
+    content = {text: content}
+  //handle patchwork style mentions.
+  var mentions = {}
+  if(Array.isArray(content.mentions))
+    content.mentions.forEach(function (link) {
+      if(link.name) mentions["@"+link.name] = link.link
+    })
+
+  var md = h('div.markdown')
+  md.innerHTML = markdown.block(content.text, {
+    toUrl: function (id) {
+      if(ref.isBlob(id)) return blob_url(id)
+      return '#'+(mentions[id]?mentions[id]:id)
+    }
+  })
+
+  return md
+
+}
+
+
+
