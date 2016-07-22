9a90757d9b3b08602cfc1aaf8d1b507d15a23d94
Dominic Tarr <dominic.tarr@gmail.com>
2016-07-22 15:50:40 +1200

    simplify index.js

--- index.js
+++ index.js
@@ -8,37 +8,10 @@ var SbotApi = require('./sbot-api')
 
 var modules = require('./modules')
 
-var renderers = []
-var app = []
-//var App = require('./plugs').first(app)
-
 var u = require('./util')
 
-//modules.unshift(SbotApi())
-//modules.unshift({app: app})
-
-//var indexes = fs.readdirSync(path.join(__dirname, 'modules'))
-//var i = indexes.indexOf('index.js')
-//indexes.splice(i, 1)
-
-//var sv = [], screen_view = require('./plugs').first(sv)
-//modules['main.js' ] = {
-//  screen_view: sv,
-//  app: function () {
-//    return h('div.row',
-//      screen_view('/public'),
-//      screen_view('/private')
-//    )
-//  }
-//}
-
-modules['app.js'] = {app: []}
-
 modules['sbot-api.js'] = SbotApi()
-
-modules['tabs.js']
-
-combine(modules) //, ['app', 'sbot'].concat(indexes) )
+combine(modules)
 
 if(process.title === 'node') {
   console.log(require('depject/graph')(modules))
@@ -49,6 +22,13 @@ document.head.appendChild(
   h('style', fs.readFileSync('./style.css', 'utf8')
 ))
 
-console.log(modules['app.js'])
-document.body.appendChild(h('div.screen.column', modules['app.js'].app[0]()))
+document.body.appendChild(modules['app.js'].app())
+
+
+
+
+
+
+
+
 
