20e18a424acad66e1f6e83ad1aa129f2fabdbb0d
Dominic Tarr <dominic.tarr@gmail.com>
2016-07-24 14:23:44 +1200

    show name and current avatar on profile page

--- modules/avatar-profile.js
+++ modules/avatar-profile.js
@@ -3,8 +3,10 @@ var plugs = require('../plugs')
 
 var avatar_image = plugs.first(exports.avatar_image = [])
 var avatar_action = plugs.map(exports.avatar_action = [])
+var avatar_edit = plugs.first(exports.avatar_edit = [])
 
 exports.avatar_profile = function (id) {
+  return avatar_edit(id)
 
   return h('div.row.profile',
     avatar_image(id),
