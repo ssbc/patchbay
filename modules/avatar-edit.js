f32aaca1352e9cd82c3584608228beda59e2a946
Dominic Tarr <dominic.tarr@gmail.com>
2016-07-24 14:51:46 +1200

    update edit to work with @ev's css

--- modules/avatar-edit.js
+++ modules/avatar-edit.js
@@ -15,12 +15,13 @@ var confirm = plugs.first(exports.message_confirm = [])
 var sbot_blobs_add = plugs.first(exports.sbot_blobs_add = [])
 var blob_url = plugs.first(exports.blob_url = [])
 var sbot_links = plugs.first(exports.sbot_links = [])
+var avatar_name = plugs.first(exports.avatar_name = [])
 
 function crop (d, cb) {
   var data
   var canvas = hypercrop(h('img', {src: d}))
 
-  return h('div.avatar_pic',
+  return h('div.row.avatar_pic',
     canvas,
     //canvas.selection,
     h('div.avatar_pic__controls',
@@ -39,7 +40,7 @@ exports.avatar_edit = function (id) {
   var img = h('img', {src: blob_url(default_avatar)})
   var lb = hyperlightbox()
   var name_input = h('input', {placeholder: 'rename'})
-  var name = h('h2.avatar__name')
+  var name = avatar_name(id)
   var selected = null
 
   getAvatar({links: sbot_links}, self_id, id, function (err, avatar) {
@@ -48,61 +49,63 @@ exports.avatar_edit = function (id) {
     if(selected) return
     if(ref.isBlob(avatar.image))
       img.src = blob_url(avatar.image)
-    name.textContent = avatar.name
   })
 
-
-  return h('div',
+  return h('div.row.profile',
     lb,
-    name,
     img,
-    name_input,
-    hyperfile.asDataURL(function (data) {
-      var el = crop(data, function (err, data) {
-        if(data) {
-          img.src = data
-          selected = dataurl.parse(data)
-        }
-        lb.close()
-      })
-      lb.show(el)
-    }),
-    h('button', 'update', {onclick: function () {
-      if(name_input.value)
-        name.textContent = name_input.value
+    h('div.column.profile__info',
+      h('strong', name),
+      name_input,
 
-      if(selected) {
-        pull(
-          pull.once(selected.data),
-          sbot_blobs_add(function (err, hash) {
-            //TODO. Alerts are EVIL.
-            //I use them only in a moment of weakness.
-            if(err) return alert(err.stack)
-            confirm({
-              type: 'about',
-              about: id,
-              name: name_input.value || undefined,
-              image: {
-                link: hash,
-                size: selected.data.length,
-                type: selected.mimetype,
-                width: 512,
-                height: 512
-              }
-            })
-          })
-        )
-      }
-      else if(input.value) //name only
-        confirm({
-          type: 'about',
-          about: id,
-          name: name_input.value || undefined,
+      hyperfile.asDataURL(function (data) {
+        var el = crop(data, function (err, data) {
+          if(data) {
+            img.src = data
+            selected = dataurl.parse(data)
+          }
+          lb.close()
         })
-      else
-        //another moment of weakness
-        alert('must select a name or image')
-    }})
+        lb.show(el)
+      }),
+      h('button', 'update', {onclick: function () {
+        if(name_input.value)
+          name.textContent = name_input.value
+
+        if(selected) {
+          pull(
+            pull.once(selected.data),
+            sbot_blobs_add(function (err, hash) {
+              //TODO. Alerts are EVIL.
+              //I use them only in a moment of weakness.
+              if(err) return alert(err.stack)
+              confirm({
+                type: 'about',
+                about: id,
+                name: name_input.value || undefined,
+                image: {
+                  link: hash,
+                  size: selected.data.length,
+                  type: selected.mimetype,
+                  width: 512,
+                  height: 512
+                }
+              })
+            })
+          )
+        }
+        else if(input.value) //name only
+          confirm({
+            type: 'about',
+            about: id,
+            name: name_input.value || undefined,
+          })
+        else
+          //another moment of weakness
+          alert('must select a name or image')
+      }})
+    )
   )
 }
+
 
