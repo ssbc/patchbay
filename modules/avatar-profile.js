fde7d979deffdab338ae5365f6fde27e502bf01a
Dominic Tarr <dominic.tarr@gmail.com>
2016-07-25 12:35:23 +1200

    show follow, unfollow link, and show follows/followers

--- modules/avatar-profile.js
+++ modules/avatar-profile.js
@@ -1,19 +1,59 @@
 var h = require('hyperscript')
 var plugs = require('../plugs')
-
+var pull = require('pull-stream')
 
 var avatar_image = plugs.first(exports.avatar_image = [])
 var avatar_name = plugs.first(exports.avatar_name = [])
 var avatar_action = plugs.map(exports.avatar_action = [])
 var avatar_edit = plugs.first(exports.avatar_edit = [])
 
-exports.avatar_profile = function (id) {
-  return avatar_edit(id)
+var follows = plugs.first(exports.follows = [])
+var followers = plugs.first(exports.followers = [])
 
-  return h('div.row.profile',
-    avatar_image(id),
-    h('div.profile__info', 
-      h('strong', avatar_name(id)), 
-        avatar_action(id))
+function streamToList(stream, el) {
+  pull(
+    stream,
+//    pull.unique(function (e) { return e }),
+    pull.drain(function (item) {
+      if(item) el.appendChild(item)
+    })
+  )
+  return el
+}
+
+function image_link (id) {
+  return h('a', {href:'#'+id}, avatar_image(id))
+}
+
+exports.avatar_profile = function (id) {
+  return h('div.column.profile',
+    avatar_edit(id),
+    avatar_action(id),
+
+      h('div.profile__relationships.column',
+
+        h('strong', 'follows'),
+        streamToList(pull(
+          follows(id),
+          pull.unique(),
+          pull.map(image_link)
+        ), h('div.profile__follows.wrap')),
+
+       h('strong', 'followers'),
+        streamToList(pull(
+          followers(id),
+          pull.unique(),
+          pull.map(image_link)
+        ), h('div.profile__followers.wrap'))
+      )
   )
 }
+
+
+
+
+
+
+
+
+
