8435ae7885131874152a7f03cd5055903c5b0906
Dominic Tarr <dominic.tarr@gmail.com>
2016-08-02 11:54:50 +1200

    only load .js files

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ console.log(
   +
   require('fs').readdirSync(require('path').join(__dirname, 'modules'))
   .filter(function (file) {
-    return file !== '_index.js'
+    return file !== '_index.js' && /\.js$/.test(file)
   })
   .map(function (file) {
     return '  '+JSON.stringify(file) + ":  require('./"+file+"')"
--- modules/index.js
+++ modules/index.js
@@ -1,7 +1,7 @@
 var fs = require('fs')
 
 fs.readdirSync(__dirname).forEach(function (e) {
-  if(e !== '_index.js')
+  if(e !== '_index.js' && /\js$/.test(e))
     exports[e] = require('./'+e)
 })
 
