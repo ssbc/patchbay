e94a95495f3b7eba6f69deb5f5ea05031b453868
Ev Bogue <ev@evbogue.com>
2016-09-19 20:15:28 +0000

    make dropdown work

--- modules/menu.js
+++ modules/menu.js
@@ -4,9 +4,9 @@ var h = require('hyperscript')
 var menu_items = plugs.map(exports.menu_items = [])
 
 var status = h('div.status.error') //start off disconnected
-  var list = h('div.column', {style: 'display: none;'})
+  var list = h('div.menu.column', {style: 'display: none;'})
 
-var menu = h('div.menu.column', status, list , {
+var menu = h('div.column', status, list , {
   onmouseover: function (e) {
     list.style.display = 'flex'
   }, onmouseout: function () {
