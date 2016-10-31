fdf74ff85a96f84b69b048224dc848094659b533
Ev Bogue <ev@evbogue.com>
2016-10-30 18:58:03 -0600

    revert to human-time defaults

--- modules/timestamp.js
+++ modules/timestamp.js
@@ -1,10 +1,5 @@
 var h = require('hyperscript')
-var _human = require('human-time')
-
-function human (date) {
-  var s =_human(date).split(' ')
-  return s[0] + (s[1] == 'month' ? 'M' : s[1][0])
-}
+var human = require('human-time')
 
 function updateTimestampEl(el) {
   el.firstChild.nodeValue = human(new Date(el.timestamp))
@@ -17,13 +12,10 @@ setInterval(function () {
 }, 60e3)
 
 exports.message_meta = function (msg) {
-  var d = new Date(msg.value.timestamp)
   return updateTimestampEl(h('a.enter.timestamp', {
     href: '#'+msg.key,
     timestamp: msg.value.timestamp,
-    title: _human(d) + '\n'+d
+    title: new Date(msg.value.timestamp)
   }, ''))
 }
-
-
 
