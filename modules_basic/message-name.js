d0b1c05b483fa6de9080159f9713c98893cdb358
Dominic Tarr <dominic.tarr@gmail.com>
2016-11-12 17:23:57 +0100

    use first line as message title

--- modules_basic/message-name.js
+++ modules_basic/message-name.js
@@ -1,15 +1,21 @@
 
 var sbot_get = require('../plugs').first(exports.sbot_get = [])
 
+function title (s) {
+  var m = /^[^\n]{0,40}/.exec(s)
+  return m && (m[0].length == 40 ? m[0]+'...' : m[0])
+}
+
 exports.message_name = function (id, cb) {
   sbot_get(id, function (err, value) {
     if(err && err.name == 'NotFoundError')
       return cb(null, id.substring(0, 10)+'...(missing)')
     if(value.content.type === 'post' && 'string' === typeof value.content.text)
-      return cb(null, value.content.text.substring(0, 40)+'...')
+      return cb(null, title(value.content.text))
     else if('string' === typeof value.content.text)
-      return cb(null, value.content.type + ':'+value.content.text.substring(0, 20))
+      return cb(null, value.content.type + ':'+title(value.content.text))
     else
       return cb(null, id.substring(0, 10)+'...')
   })
 }
+
