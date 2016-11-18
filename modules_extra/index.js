043f0016a782ff805a586313f336b144a0ed3e3e
cel <cel@f/6sQ6d2CMxRUhLpspgGIulDxDCwYD7DzFzPNr7u5AU=.ed25519>
2016-11-18 14:57:57 -0500

    Add dns record message renderer

--- modules_extra/index.js
+++ modules_extra/index.js
@@ -3,6 +3,7 @@ module.exports = {
   "channel.js":  require('./channel.js'),
   "emoji.js":  require('./emoji.js'),
   "suggest-emoji.js":  require('./suggest-emoji.js'),
+  "dns.js":  require('./dns.js'),
   "git.js":  require('./git.js'),
   "notifications.js":  require('./notifications.js'),
   "meta-image.js":  require('./meta-image.js'),
