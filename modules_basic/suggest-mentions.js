944aabb813d4b39e34dc79a025bb30fa8db414ec
cel <cel@f/6sQ6d2CMxRUhLpspgGIulDxDCwYD7DzFzPNr7u5AU=.ed25519>
2016-11-23 02:13:03 -0500

    Refactor suggestions
    - Use async plug combiner to get suggestions for mentions & search.
    - Move channel suggestions into channel.js

--- modules_basic/suggest-mentions.js
+++ modules_basic/suggest-mentions.js
@@ -1,5 +1,4 @@
 var pull = require('pull-stream')
-var cont = require('cont')
 function isImage (filename) {
   return /\.(gif|jpg|png|svg)$/i.test(filename)
 }
@@ -8,7 +7,7 @@ var sbot_links2 = require('../plugs').first(exports.sbot_links2 = [])
 var blob_url = require('../plugs').first(exports.blob_url = [])
 var signified = require('../plugs').first(exports.signified = [])
 
-exports.suggest = cont.to(function (word, cb) {
+exports.suggest_mentions = function (word, cb) {
   if(!/^[%&@]\w/.test(word)) return cb()
 
 
@@ -23,8 +22,38 @@ exports.suggest = cont.to(function (word, cb) {
       }
     }))
   })
+}
+
+//TODO: this list should be generated from plugs
+var builtinTabs = [
+  '/public', '/private', '/notifications',
+  '/network', '/query', '/versions'
+].map(function (name) {
+  return {
+    title: name,
+    value: name,
+  }
 })
 
+exports.suggest_search = function (query, cb) {
+  if(/^[@%]\w/.test(query)) {
+    signified(query, function (_, names) {
+      cb(null, names.map(function (e) {
+        return {
+          title: e.name + ':'+e.id.substring(0, 10),
+          value: e.id,
+          subtitle: e.rank,
+          rank: e.rank
+        }
+      }))
+    })
+
+  } else if(/^\//.test(query)) {
+    cb(null, builtinTabs.filter(function (name) {
+      return name.value.substr(0, query.length) === query
+    }))
+  } else cb()
+}
 
 
 
