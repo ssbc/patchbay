4f3054d52e3dd8118e872578e5b857dcdad40d59
Michael Williams <michael.williams@enspiral.com>
2016-12-28 22:57:57 +1300

    fix backlink rendering

--- modules_basic/message-backlinks.js
+++ modules_basic/message-backlinks.js
@@ -1,7 +1,19 @@
-exports.gives = 'message_backlinks'
+var fs = require('fs')
+var Path = require('path')
+var h = require('../h')
+
+exports.gives = {
+  message_backlinks: true,
+  mcss: true
+}
 
 exports.create = function (api) {
-  return function (msg) {
+  return {
+    message_backlinks,
+    mcss: () => fs.readFileSync(Path.join(__dirname, 'message-backlinks.mcss'))
+  }
+
+  function message_backlinks (msg) {
     var links = []
     for(var k in CACHE) {
       var _msg = CACHE[k]
@@ -11,6 +23,18 @@ exports.create = function (api) {
           links.push(k)
       }
     }
-    return links
+
+    if (links.length === 0) return null
+
+    return h('MessageBacklinks', [
+      h('header', 'backlinks:'),
+      h('ul', links.map(function (link) {
+        return h('li', [
+          h('a -backlink', {
+            href: `#${link}`
+          }, link)
+        ])
+      }))
+    ])
   }
 }
