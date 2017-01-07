83811f3eeab7f9cae46ca83ffe34c19b2974a4ac
mix irving <whimful@gmail.com>
2017-01-07 13:48:25 +1300

    styled network page, NOW WITH LABELS!

--- modules_basic/avatar.js
+++ modules_basic/avatar.js
@@ -10,28 +10,36 @@ exports.needs = {
 exports.gives = {
   avatar: true,
   avatar_image_name_link: true,
-  avatar_image_link: true
+  avatar_image_link: true,
+  avatar_name_link: true
 }
 
 exports.create = function (api) {
+  return {
+    avatar,
+    avatar_image_name_link,
+    avatar_image_link,
+    avatar_name_link
+  }
 
-  var exports = {}
-  exports.avatar = function (author, classes) {
+  function avatar (author, classes) {
     return exports.avatar_image_name_link(author, classes)
   }
 
-  exports.avatar_image_name_link = function (author, classes) {
+  function avatar_image_name_link (author, classes) {
     return api.avatar_link(author, [
       api.avatar_image(author, classes),
       api.avatar_name(author)
     ])
   }
 
-  exports.avatar_image_link = function (author, classes) {
+  function avatar_image_link (author, classes) {
     return api.avatar_link(author, api.avatar_image(author, classes))
   }
 
-  return exports
+  function avatar_name_link (author, classes) {
+    return api.avatar_link(author, api.avatar_name(author))
+  }
 }
 
 
