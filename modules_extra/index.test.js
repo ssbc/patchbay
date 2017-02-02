62b1b715518a0191568675f9f904c90672126009
mix irving <whimful@gmail.com>
2017-02-02 14:56:01 +1300

    tidy up unused test setup

--- modules_extra/index.test.js
+++ modules_extra/index.test.js
@@ -1,5 +1,4 @@
 const test = require('tape')
-const jsdom = require('jsdom')
 const combine = require('depject')
 
 process.env.ssb_appname = 'test'
@@ -8,13 +7,8 @@ const core = require('../modules_core')
 const basic = require('../modules_basic')
 const extra = require('./')
 
-// jsdom.env('<body></body>', (err, window) => {
-
-  test('modules_extra has no outside deps', t => {
-    t.ok(combine(extra, basic, core))
-    t.end()
-    // window.close()
-  })
-// })
-
+test('modules_extra has no outside deps', t => {
+  t.ok(combine(extra, basic, core))
+  t.end()
+})
 
