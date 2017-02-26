0a1c7842563bee8ca52221c858cfe7309da160da
mix irving <whimful@gmail.com>
2017-02-26 15:01:23 +1300

    minimal private message feed

--- router/html/page/private.js
+++ router/html/page/private.js
@@ -1,13 +1,52 @@
 const { h } = require('mutant')
 const nest = require('depnest')
+const pull = require('pull-stream')
+const Scroller = require('pull-scroll')
+const next = require('../../../junk/next-stepper')
+const ref = require('ssb-ref')
 
 exports.gives = nest('router.html.page')
 
+exports.needs = nest({
+  'feed.pull.private': 'first',
+  'keys.sync.id': 'first',
+  'main.html.scroller': 'first',
+  'message.html': {
+    compose: 'first',
+    render: 'first',
+  },
+})
+
 exports.create = function (api) {
-  return nest('router.html.page', (path) => {
+  return nest('router.html.page', privatePage)
+  
+  function privatePage (path) {
     if (path !== '/private') return
 
-    return h('div.private', 'private')
-  })
+    const id = api.keys.sync.id()
+
+    const composer = api.message.html.compose({
+      meta: { type: 'post' }, 
+      prepublish: meta => {
+        meta.recps = [id, ...meta.mentions]
+          .filter(m => ref.isFeed(typeof m  === 'string' ? m : m.link))
+        return meta
+      },
+      placeholder: 'Write a private message. \n\n@mention users in the first message to start a private thread.'}
+    )
+    const { container, content } = api.main.html.scroller({ prepend: composer })
+
+    pull(
+      next(api.feed.pull.private, {old: false, limit: 100}),
+      Scroller(container, content, api.message.html.render, true, false)
+    )
+
+    pull(
+      next(api.feed.pull.private, {reverse: true, limit: 100, live: false}),
+      Scroller(container, content, api.message.html.render, false, false)
+    )
+
+    return container
+  }
 }
 
--- router/html/page/public.js
+++ router/html/page/public.js
@@ -2,13 +2,12 @@ const { h } = require('mutant')
 const nest = require('depnest')
 const pull = require('pull-stream')
 const Scroller = require('pull-scroll')
-
 const next = require('../../../junk/next-stepper')
 
 exports.gives = nest('router.html.page')
 
 exports.needs = nest({
-  'sbot.pull.log': 'first',
+  'feed.pull.public': 'first',
   'message.html': {
     compose: 'first',
     render: 'first',
@@ -17,23 +16,25 @@ exports.needs = nest({
 })
 
 exports.create = function (api) {
-  return nest('router.html.page', (path) => {
+  return nest('router.html.page', publicPage)
+    
+  function publicPage (path) {
     if (path !== '/public') return
 
     const composer = api.message.html.compose({ meta: { type: 'post' }, placeholder: 'Write a public message'})
-    var { container, content } = api.main.html.scroller({ prepend: composer })
+    const { container, content } = api.main.html.scroller({ prepend: composer })
 
     pull(
-      next(api.sbot.pull.log, {old: false, limit: 100}),
+      next(api.feed.pull.public, {old: false, limit: 100}),
       Scroller(container, content, api.message.html.render, true, false)
     )
 
     pull(
-      next(api.sbot.pull.log, {reverse: true, limit: 100, live: false}),
+      next(api.feed.pull.public, {reverse: true, limit: 100, live: false}),
       Scroller(container, content, api.message.html.render, false, false)
     )
 
     return container
-  })
+  }
 }
 
