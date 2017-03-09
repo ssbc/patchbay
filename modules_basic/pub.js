1900c83cc548688cc1df8a623f9c411441052a86
cel <cel@f/6sQ6d2CMxRUhLpspgGIulDxDCwYD7DzFzPNr7u5AU=.ed25519>
2017-03-08 22:25:40 -0500

    Render pub messages smaller
    also, support alternative pub schema

--- modules_basic/pub.js
+++ modules_basic/pub.js
@@ -5,20 +5,20 @@ exports.needs = {
   avatar_link: 'first'
 }
 
-exports.gives = 'message_content'
+exports.gives = 'message_content_mini'
 
 exports.create = function (api) {
 
   return function (msg, sbot)  {
     var c = msg.value.content
     if (c.type === 'pub') {
-      var address = c.address || {}
+      var address = c.address || c.pub || {}
+      var pubId = address.key || address.link
       return [
-        h('p', 'announced an address for ',
-          api.avatar_link(address.key, api.avatar_name(address.key)), ':'),
-        h('blockquote',
-          h('code', address.host + ':' + address.port)
-        )
+        'connects to ',
+        api.avatar_link(pubId, api.avatar_name(pubId)),
+        ' at ',
+        h('code', address.host, address.port ? ':' + address.port : '')
       ]
     }
   }
