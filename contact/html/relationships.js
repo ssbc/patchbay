f441f7e134ae1555c42d5c0bc58be4bd414e97cd
mix irving <whimful@gmail.com>
2017-03-17 11:26:46 +1300

    add follow/ unfollow to profiles

--- contact/html/relationships.js
+++ contact/html/relationships.js
@@ -1,5 +1,5 @@
 const nest = require('depnest')
-const { h, map, computed } = require('mutant')
+const { h, map, computed, when } = require('mutant')
 
 exports.gives = nest('contact.html.relationships')
 
@@ -9,12 +9,16 @@ exports.needs = nest({
     'obs.name': 'first'
   },
   contact: {
+    async: {
+      follow: 'first',
+      unfollow: 'first'
+    },
     obs: {
       followers: 'first',
       following: 'first'
     }
-    // TODO add following
-  }
+  },
+  'keys.sync.id': 'first'
 })
 
 exports.create = function (api) {
@@ -29,12 +33,23 @@ exports.create = function (api) {
     var friends = computed([rawFollowing, rawFollowers], (following, followers) => {
       return [...following].filter(follow => followers.has(follow))
     })
-
     var following = computed([rawFollowing, friends], (following, friends) => {
       return [...following].filter(follow => !friends.includes(follow))
     })
     var followers = computed([rawFollowers, friends], (followers, friends) => {
       return [...followers].filter(follower => !friends.includes(follower))
+    })
+
+    var myId = api.keys.sync.id()
+    var ImFollowing = api.contact.obs.following(myId)
+    var IFollowThem = computed([ImFollowing], ImFollowing => ImFollowing.has(id))
+    var theyFollowMe = computed([rawFollowing], following => following.has(myId))
+
+    var relationshipStatus = computed([IFollowThem, theyFollowMe], (IFollowThem, theyFollowMe) => {
+      return IFollowThem && theyFollowMe ? '- you are friends'
+        : IFollowThem ? '- you follow them'
+        : theyFollowMe ? '- they follow you'
+        : ''
     })
 
     function imageLink (id) {
@@ -44,13 +59,23 @@ exports.create = function (api) {
       )
     }
 
-    // TOOD - split this into relationships, move top level stuff into Profile
     return h('Relationships', [
       h('header', 'Relationships'),
-      h('div.your-status', [
-        h('header', 'Your status')
-        // h('section.action', api.contact.action(id))
-      ]),
+      when(id !== myId, 
+        h('div.your-status', [
+          h('header', 'Your status'),
+          h('section.action', [
+            when(ImFollowing.sync, 
+              when(IFollowThem, 
+                h('button', { 'ev-click': api.contact.async.unfollow }, 'Unfollow'),
+                h('button', { 'ev-click': api.contact.async.follow }, 'Follow')
+              ),
+              h('button', { disabled: 'disabled' }, 'Loading...')
+            ),
+          ]),
+          when(ImFollowing.sync, h('section.status', relationshipStatus))
+        ])
+      ),
       h('div.friends', [
         h('header', 'Friends'),
         h('section', map(friends, imageLink))
