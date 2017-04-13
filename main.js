cc90594fd56722af3541a8d8e5509974138059ae
Michael Williams <michael.williams@enspiral.com>
2017-04-13 22:42:48 +1200

    support `patchbay` to be consumed by `patchbay-lite`
    - export depject modules for external consumption
    - add browserify transforms when consuming module
    - remove `electro` and `electron` dependencies
    - fix up some documentation
    
    https://github.com/ssbc/patchbay/pull/100

---
+++ main.js
@@ -0,0 +1,25 @@
+const combine = require('depject')
+const entry = require('depject/entry')
+const nest = require('depnest')
+
+// const git = require('patch-git')
+const horcrux = require('ssb-horcrux')
+const patchbay = require('./')
+const patchcore = require('patchcore')
+
+// polyfills
+require('setimmediate')
+
+// from more specialized to more general
+const sockets = combine(
+  // git,
+  horcrux,
+  patchbay,
+  patchcore
+)
+
+const api = entry(sockets, nest('app.html.app', 'first'))
+
+const app = api.app.html.app()
+document.body.appendChild(app)
+
