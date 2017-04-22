b7d7f86afc99c01161ac2e00a2decf8ecb85246e
mix irving <whimful@gmail.com>
2017-04-23 11:17:21 +1200

    extract addPage and goTo into sync functions

---
+++ app/sync/goTo.js
@@ -0,0 +1,30 @@
+const nest = require('depnest')
+
+exports.gives = nest({ 'app.sync.goTo': true })
+
+exports.needs = nest({
+  'app.html.tabs': 'first',
+  'app.sync.addPage': 'first' 
+})
+
+exports.create = function (api) {
+  return nest({
+    'app.sync': {
+      goTo
+    }
+  })
+
+  function goTo (path, change) {
+    const tabs = api.app.html.tabs()
+
+    if (tabs.has(path)) {
+      tabs.select(path)
+      return true
+    }
+
+    addPage(path, true, false)
+    return change
+  }
+
+}
+
