761ea73ffeb6b94dc77fdb5aa47beeda1396d892
mix irving <whimful@gmail.com>
2017-04-29 23:28:52 +1200

    fix menu

--- app/html/menu.js
+++ app/html/menu.js
@@ -3,22 +3,28 @@ const { h, Value } = require('mutant')
 
 exports.gives = nest('app.html.menu')
 
-exports.needs = nest('app.html.menuItem', 'map')
+exports.needs = nest({
+  app: {
+    'html.menuItem': 'map',
+    'sync.goTo': 'first'
+  }
+})
 
 exports.create = function (api) {
   var _menu
 
-  return nest('app.html.menu', function menu (handleClick) {
+  return nest('app.html.menu', function menu () {
     if (_menu) return _menu
 
     var state = Value('')
 
+    // TODO: move goTo out into each menuItem
     _menu = h('Menu', {
       classList: [ state ],
       'ev-mouseover': () => state.set('-active'),
       'ev-mouseout': () => state.set('')
     }, [
-      h('div', api.app.html.menuItem(handleClick))
+      h('div', api.app.html.menuItem(api.app.sync.goTo))
     ])
 
     return _menu
