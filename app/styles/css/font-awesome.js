04c385c3b68e8f6a953b37f9f099b1116e22c8b5
Mikey <michael.williams@enspiral.com>
2017-06-05 01:02:09 +1200

    update font-awesome.js to be patchlite friendly
    - code shouldn't be run outside `create` function
    - use [`require-style`](https://github.com/ahdinosaur/require-style)!

--- app/styles/css/font-awesome.js
+++ app/styles/css/font-awesome.js
@@ -1,20 +1,11 @@
 const nest = require('depnest')
-const fs = require('fs')
-const { join, dirname } = require('path')
+const requireStyle = require('require-style')
 const { assign } = Object
-const styleResolve = require('style-resolve')
-
-const fontCssPath = styleResolve.sync('font-awesome')
-const css = fs.readFileSync(fontCssPath, 'utf8')
-  .replace(/\.{2}/g, (match) => join(dirname(fontCssPath), match))
-
-// TODO: for patchlite, may have to convert font urls into url(data:base64: ....) format
 
 exports.gives = nest('styles.css')
 
 exports.create = function (api) {
   return nest('styles.css', (sofar = {}) => {
-    return assign(sofar, { fontAwesome: css })
+    return assign(sofar, { css: requireStyle('font-awesome') })
   })
 }
-
