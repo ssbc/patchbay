3e3f07cbc0498c3ae32ce74644033ff91daae787
Gordon Martin <gordon.hugh.martin@gmail.com>
2017-07-18 22:22:02 +0100

    Introducing chess :o

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ const bayGatherings = require('patchbay-gatherings')
 const patchbay = require('./')
 const patchcore = require('patchcore')
 
+const ssbchess = require('ssb-chess')
+
 // polyfills
 require('setimmediate')
 
@@ -21,7 +23,8 @@ const sockets = combine(
   gatherings,
   horcrux,
   patchbay,
-  patchcore
+  patchcore,
+  ssbchess
 )
 
 const api = entry(sockets, nest('app.html.app', 'first'))
