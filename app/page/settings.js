32f9e6a107ea72ec7997ced62ff2ed0614158b46
Anders Rune Jensen <arj03@protonmail.ch>
2018-05-30 22:05:12 +0200

    Add ability to remove exif metadata from images

--- app/page/settings.js
+++ app/page/settings.js
@@ -8,6 +8,7 @@ exports.gives = nest({
 
 exports.needs = nest({
   'app.html.settings': 'map',
+  'app.html.scroller': 'first',
   'app.sync.goTo': 'first'
 })
 
@@ -25,7 +26,7 @@ exports.create = function (api) {
   }
 
   function publicPage (location) {
-    return h('SettingsPage', { title: '/settings' }, [
+    var page = h('SettingsPage', { title: '/settings' }, [
       h('div.container', [
         h('h1', 'Settings'),
         api.app.html.settings().map(setting => {
@@ -38,5 +39,9 @@ exports.create = function (api) {
         })
       ])
     ])
+
+    var { container, content } = api.app.html.scroller({ prepend: page })
+    container.title = 'Settings'
+    return container
   }
 }
