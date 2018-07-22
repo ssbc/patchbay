368b6fabc6c25007c87d6e7a601888a0dd21c561
mixmix <whimful@gmail.com>
2018-07-22 22:30:31 +1200

    /calendar : have selection window display friggen gatherings !

--- app/page/calendar.js
+++ app/page/calendar.js
@@ -1,11 +1,13 @@
 const nest = require('depnest')
-const { h, Array: MutantArray, Struct, computed, watch, throttle } = require('mutant')
+const { h, Array: MutantArray, map, Struct, computed, watch, throttle, resolve } = require('mutant')
 const pull = require('pull-stream')
 const { isMsg } = require('ssb-ref')
 
 exports.gives = nest('app.page.calendar')
 
 exports.needs = nest({
+  'message.html.render': 'first',
+  'sbot.async.get': 'first',
   'sbot.pull.stream': 'first'
 })
 
@@ -17,14 +19,52 @@ exports.create = (api) => {
     const state = Struct({
       today: new Date(d.getFullYear(), d.getMonth(), d.getDate()),
       year: d.getFullYear(),
-      events: MutantArray([])
+      events: MutantArray([]),
+      range: Struct({
+        gte: new Date(d.getFullYear(), d.getMonth(), d.getDate()),
+        lt: new Date(d.getFullYear(), d.getMonth(), d.getDate() + 1)
+      })
     })
 
     watch(state.year, year => getEvents(year, state.events))
 
-    return h('CalendarPage', { title: '/calendar' }, [
-      Calendar(state)
+    const page = h('CalendarPage', { title: '/calendar' }, [
+      Calendar(state),
+      Events(state)
     ])
+
+    page.scroll = i => {
+      const gte = resolve(state.range.gte)
+      state.range.gte.set(new Date(gte.getFullYear(), gte.getMonth(), gte.getDate() + i))
+      const lt = resolve(state.range.lt)
+      state.range.lt.set(new Date(lt.getFullYear(), lt.getMonth(), lt.getDate() + i))
+    }
+
+    return page
+  }
+
+  function Events (state) {
+    return h('CalendarEvents', computed([state.events, state.range], (events, range) => {
+      const keys = events
+        .filter(ev => ev.date >= range.gte && ev.date < range.lt)
+        .sort((a, b) => a.date - b.date)
+        .map(ev => ev.data.key)
+
+      const gatherings = MutantArray([])
+
+      pull(
+        pull.values(keys),
+        pull.asyncMap((key, cb) => {
+          api.sbot.async.get(key, (err, value) => {
+            if (err) return cb(err)
+            cb(null, {key, value})
+          })
+        }),
+        pull.drain(msg => gatherings.push(msg))
+      )
+
+      return map(gatherings, g => api.message.html.render(g))
+    }))
   }
 
   function getEvents (year, events) {
@@ -82,11 +122,13 @@ function Calendar (state) {
   // TODO assert events is an Array of object
   // of form { date, data }
 
+  const setRange = state.range.set
+
   return h('Calendar', [
     Header(state.year),
-    h('div.months', computed(throttle(state, 100), ({ today, year, events }) => {
+    h('div.months', computed(throttle(state, 100), ({ today, year, events, range }) => {
       return MONTHS.map((month, monthIndex) => {
-        return Month({ month, monthIndex, today, year, events })
+        return Month({ month, monthIndex, today, year, events, range, setRange })
       })
     }))
   ])
@@ -102,20 +144,23 @@ function Header (year) {
   ])
 }
 
-function Month ({ month, monthIndex, today, year, events }) {
+function Month ({ month, monthIndex, today, year, events, range, setRange }) {
   const monthLength = new Date(year, monthIndex + 1, 0).getDate()
   // NOTE Date takes month as a monthIndex i.e. april = 3
   // and day = 0 goes back a day
   const days = Array(monthLength).fill().map((_, i) => i + 1)
 
-  var date
-  var dateEnd
   var weekday
   var week
   var offset = getDay(new Date(year, monthIndex, 1)) - 1
 
+  const setMonthRange = () => setRange({
+    gte: new Date(year, monthIndex, 1),
+    lt: new Date(year, monthIndex + 1, 1)
+  })
+
   return h('CalendarMonth', [
-    h('div.month-name', month.substr(0, 2)),
+    h('div.month-name', { 'ev-click': setMonthRange }, month.substr(0, 2)),
     h('div.days', { style: {display: 'grid'} }, [
       DAYS.map((day, i) => DayName(day, i)),
       days.map(Day)
@@ -123,8 +168,8 @@ function Month ({ month, monthIndex, today, year, events }) {
   ])
 
   function Day (day) {
-    date = new Date(year, monthIndex, day)
-    dateEnd = new Date(year, monthIndex, day + 1)
+    const date = new Date(year, monthIndex, day)
+    const dateEnd = new Date(year, monthIndex, day + 1)
     weekday = getDay(date)
     week = Math.ceil((day + offset) / 7)
 
@@ -144,15 +189,23 @@ function Month ({ month, monthIndex, today, year, events }) {
       },
       classList: [
         date < today ? '-past' : '-future',
-        eventsOnDay.length ? '-events' : ''
+        eventsOnDay.length ? '-events' : '',
+        date >= range.gte && date < range.lt ? '-selected' : ''
       ]
     }
 
     if (!eventsOnDay.length) return h('CalendarDay', opts)
 
+    opts['ev-click'] = () => setRange({
+      gte: date,
+      lt: dateEnd
+    })
+    opts['ev-hover'] = () => console.log(date)
+
     return h('CalendarDay', opts, [
+      // TODO add awareness of whether I'm going to events
       // TODO try a FontAwesome circle
-      h('div', [
+      h('div.dot', [
         // Math.random() > 0.3 ? h('div') : ''
       ])
     ])
@@ -175,4 +228,3 @@ function getDay (date) {
   // Weeks run 0...6 (Sun - Sat)
   // this shifts those days around by 1
 }
-
