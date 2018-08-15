e0d1f2605dbd0f6b655b690d173b21a6de2d11f1
Christian Bundy <christianbundy@fraction.io>
2018-08-15 12:05:36 -0700

    Remove when() on non-observables

--- message/html/render/about.js
+++ message/html/render/about.js
@@ -43,9 +43,9 @@ exports.create = function (api) {
     about = about || link
 
     const metaData = [
-      when(name, h('div', [ h('strong', 'Name: '), name ])),
-      when(description, h('div', [ h('strong', 'Description: '), description ])),
-      when(image, h('img', { src: api.blob.sync.url(image), style: { 'margin-top': '.5rem' } }))
+      name ? h('div', [ h('strong', 'Name: '), name ]) : undefined,
+      description ? h('div', [ h('strong', 'Description: '), description ]) : undefined,
+      image ? h('img', { src: api.blob.sync.url(image), style: { 'margin-top': '.5rem' } }): undefined
     ]
 
     if (!ref.isFeed(about)) {
