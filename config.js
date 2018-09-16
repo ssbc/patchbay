e5f2286157811d60f297d5d13d24b72ba1b62b3d
Anders Rune Jensen <arj03@protonmail.ch>
2018-09-16 21:06:13 +0200

    Add tor-only outgoing connections to settings. Currently tor doesn't actually work with npm start because it can't connect to proxy.

--- config.js
+++ config.js
@@ -3,6 +3,8 @@ const Config = require('ssb-config/inject')
 const ssbKeys = require('ssb-keys')
 const Path = require('path')
 const merge = require('lodash/merge')
+// settings not available in api yet, so we need to load it manually
+const settings = require('patch-settings').patchSettings
 
 const appName = process.env.ssb_appname || 'ssb'
 const opts = appName === 'ssb'
@@ -19,6 +21,18 @@ exports.create = (api) => {
 
       const keys = ssbKeys.loadOrCreateSync(Path.join(config.path, 'secret'))
       const pubkey = keys.id.slice(1).replace(`.${keys.curve}`, '')
+
+      if (settings.create().settings.sync.get('patchbay.torOnly', false)) {
+        merge(config, {
+          connections: {
+            outgoing: {
+              "onion": [{ "transform": "shs" }]
+            }
+          }
+        })
+
+        delete config.connections.outgoing.net
+      }
 
       config = merge(config, {
         connections: {
