707b786ad8b53851c240262677d2e164cf75bf59
mixmix <whimful@gmail.com>
2018-09-19 15:14:04 +1200

    teach connection thing in corner to blink when new messages are saved

--- app/html/menu.js
+++ app/html/menu.js
@@ -1,12 +1,14 @@
 const nest = require('depnest')
 const { h, Value, when } = require('mutant')
+const pull = require('pull-stream')
 
 exports.gives = nest('app.html.menu')
 
 exports.needs = nest({
   'app.html.menuItem': 'map',
   'app.sync.goTo': 'first',
-  'sbot.obs.connection': 'first'
+  'sbot.obs.connection': 'first',
+  'sbot.pull.stream': 'first'
 })
 
 exports.create = function (api) {
@@ -17,6 +19,33 @@ exports.create = function (api) {
 
     const hoverClass = Value('')
     const connectionClass = when(api.sbot.obs.connection, '', '-disconnected')
+    const newMessageClass = Value('')
+
+    var timeOut
+    pull(
+      api.sbot.pull.stream(sbot => {
+        const query = [{
+          $filter: {
+            timestamp: { $gt: 0 }
+          }
+        }, {
+          $map: {
+            author: ['value', 'author']
+          }
+        }]
+        return sbot.query.read({ live: true, old: false, query })
+      }),
+      // pull.filter(a => a !== myKey), // could filter out my own messages
+      pull.drain(m => {
+        if (timeOut) return
+
+        newMessageClass.set('-newMsg')
+        timeOut = setTimeout(() => {
+          newMessageClass.set('')
+          timeOut = null
+        }, 200)
+      })
+    )
 
     const menuItems = api.app.html.menuItem(api.app.sync.goTo).map(item => {
       // Remove custom order from dependencies that give app.html.menuItem
@@ -29,7 +58,7 @@ exports.create = function (api) {
 
     // TODO: move goTo out into each menuItem
     _menu = h('Menu', {
-      classList: [ hoverClass, connectionClass ],
+      classList: [ hoverClass, connectionClass, newMessageClass ],
       'ev-mouseover': () => hoverClass.set('-open'),
       'ev-mouseout': () => hoverClass.set('')
     }, [
