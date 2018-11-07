91c8b62421b640cbcb2d00b774aa3cfde721a08d
mixmix <whimful@gmail.com>
2018-11-07 14:17:42 +1300

    bump patchbay-scry to add /scry view

--- app/html/search-bar.js
+++ app/html/search-bar.js
@@ -74,7 +74,7 @@ exports.create = function (api) {
     function getPagesSuggestions (word) {
       const pages = [
         'blogs', 'calendar', 'posts', 'public', 'private', 'inbox', 'profile', 'notifications', 'settings', 'shortcuts',
-        'gatherings', 'chess', 'books', 'imageSearch', 'polls', 'query', 'dark-crystal', 'postRank', 'scry/new'
+        'gatherings', 'chess', 'books', 'imageSearch', 'polls', 'query', 'dark-crystal', 'postRank', 'scry', 'scry/new'
       ]
 
       return pages
