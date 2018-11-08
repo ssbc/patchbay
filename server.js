8c46c33680cf091b5042e24c83fa18a328250db7
mixmix <whimful@gmail.com>
2018-11-08 16:47:25 +1300

    have server check for manifest before trying a connection

--- server.js
+++ server.js
@@ -1,23 +1,39 @@
+const fs = require('fs')
+const { join } = require('path')
 const Client = require('ssb-client')
 const scuttleshell = require('scuttle-shell')
 const electron = require('electron')
 
 // Get config options from depject
 const config = require('./config').create().config.sync.load()
-const startFrontend = () => electron.ipcRenderer.send('server-started')
 
-Client(config.keys, config, (err, server) => {
-  if (err) { // no server currently running
-    console.log('> scuttle-shell: starting')
-    scuttleshell.start({}, (startErr) => {
-      if (startErr) return console.error('> scuttle-shell: failed to start', startErr)
+// check if manifest.json exists (has any sbot ever started?)
+if (!fs.existsSync(join(config.path, 'manifest.json'))) startScuttleShell()
+else {
+  // check if there's a server running we can connect to
+  Client(config.keys, config, (err, server) => {
+    if (err) startScuttleShell()
+    else {
+      console.log('> scuttle-shell / sbot already running')
+      server.close() // close this connection (app starts one of its own)
 
       startFrontend()
-    })
-  } else {
-    console.log('> scuttle-shell / sbot already running')
-    server.close() // close this connection (app starts one of its own)
+    }
+  })
+}
+
+// helpers
+
+function startScuttleShell () {
+  console.log('> scuttle-shell: starting')
+
+  scuttleshell.start({}, (startErr) => {
+    if (startErr) return console.error('> scuttle-shell: failed to start', startErr)
 
     startFrontend()
-  }
-})
+  })
+}
+
+function startFrontend () {
+  electron.ipcRenderer.send('server-started')
+}
