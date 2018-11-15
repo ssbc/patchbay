577868d4ce80d09576a9520cb28142f2136a4577
Henry <cryptix@riseup.net>
2018-11-15 08:49:09 +0100

    tweak the config to get things working (may need reverting)

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const scuttleshell = require('scuttle-shell')
 
 // Get config options from depject
 const config = require('./config').create().config.sync.load()
+const startFrontend = () => electron.ipcRenderer.send('server-started')
 
 // Check if scuttle-shell is already running
 // TODO - make this check for scuttle-shell specifically (and not just an sbot)
@@ -13,16 +14,14 @@ Client(config.keys, config, (err, server) => {
     console.warn('client connection failed:', err)
     console.log('> starting scuttle-shell')
     scuttleshell.start({}, (startErr) => {
-	  console.log('start done!', startErr)
-      if (startErr) {
-        console.error('failed to start scuttle-shell:', startErr)
-      } else {
-        electron.ipcRenderer.send('server-started')
-      }
+      if (startErr) return console.error('> scuttle-shell: failed to start', startErr)
+
+      startFrontend()
     })
   } else {
     console.log('> scuttle-shell / sbot already running')
     server.close() // close this connection (app starts one of its own)
-    electron.ipcRenderer.send('server-started')
+
+    startFrontend()
   }
 })
