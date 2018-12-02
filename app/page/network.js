3641823ab81be8b64bfe575a2635cbbd6a5bb05d
mixmix <whimful@gmail.com>
2018-12-02 20:44:19 +1300

    /network : fix graph updating

--- app/page/network.js
+++ app/page/network.js
@@ -1,5 +1,5 @@
 const nest = require('depnest')
-const { h, Value, Dict, onceTrue, computed, watchAll, throttle } = require('mutant')
+const { h, Value, Dict, onceTrue, computed, watch, watchAll, throttle } = require('mutant')
 const Chart = require('chart.js')
 const pull = require('pull-stream')
 
@@ -45,9 +45,8 @@ exports.create = function (api) {
     const latest = Value(toTimeBlock(Date.now(), minsPerStep))
     // start of the most recent bar
     setInterval(() => {
-      console.log('boop')
       latest.set(toTimeBlock(Date.now(), minsPerStep))
-    }, 2 * MINUTE)
+    }, minsPerStep / 4 * MINUTE)
 
     const range = computed([latest], (latest) => {
       return {
@@ -109,44 +108,36 @@ function getData ({ data, server, minsPerStep, scale }) {
 function initialiseChart ({ canvas, data, range }) {
   var chart = new Chart(canvas.getContext('2d'), chartConfig(range))
 
-  const chartData = computed([throttle(data, 300), range], (data, range) => {
-    return Object.keys(data)
-      .filter(ts => ts >= range.lower && ts < range.upper)
+  watch(range, ({ lower, upper }) => {
+    // set horizontal scale
+    chart.options.scales.xAxes[0].time.min = lower
+    chart.options.scales.xAxes[0].time.max = upper
+    chart.update()
+  })
+
+  watchAll([throttle(data, 300), range], (data, { lower, upper }) => {
+    const _data = Object.keys(data)
+      .sort((a, b) => a < b ? -1 : +1)
       .map(ts => {
         return {
           t: Number(ts), // NOTE - might need to offset by a half-step ?
           y: data[ts]
         }
       })
-  })
-  chartData(data => {
-    chart.data.datasets[0].data = data
-    chart.update()
-  })
 
-  // Scales the height of the graph (to the visible data)!
-  watchAll([chartData, range], (chartData, range) => {
-    const { lower, upper } = range
-    const slice = chartData
-      .filter(d => d.t >= lower && d.t < upper) // unnecessary ??
+    // update chard data
+    chart.data.datasets[0].data = _data
+
+    // scales the height of the graph (to the visible data)!
+    const slice = _data
+      .filter(d => d.t >= lower && d.t < upper)
       .map(d => d.y)
       .sort((a, b) => a > b ? -1 : +1)
 
     var h = slice[0]
     if (!h || h < GRAPH_Y_MIN) h = GRAPH_Y_MIN // min-height
     else h = h + (GRAPH_Y_STEP - h % GRAPH_Y_STEP) // round height to multiples of GRAPH_Y_STEP
-
     chart.options.scales.yAxes[0].ticks.max = h
-
-    chart.update()
-  })
-
-  // Update the x-axes bounds of the graph!
-  range(range => {
-    const { lower, upper } = range
-
-    chart.options.scales.xAxes[0].time.min = lower
-    chart.options.scales.xAxes[0].time.max = upper
 
     chart.update()
   })
