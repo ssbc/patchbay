c2c3592f96ca4dddb067997ed7f1a2b45ab7b5bb
mixmix <whimful@gmail.com>
2019-04-27 17:56:04 +1200

    remove un-used needs from initialise/dat

--- app/sync/initialise/dat.js
+++ app/sync/initialise/dat.js
@@ -3,11 +3,6 @@ const datSharedFiles = require('dat-shared-files/lib')
 
 exports.gives = nest('app.sync.initialise')
 
-exports.needs = nest({
-  'router.sync.router': 'first',
-  'app.html.tabs': 'first'
-})
-
 exports.create = function (api) {
   return nest('app.sync.initialise', datShare)
 
